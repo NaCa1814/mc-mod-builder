55657ba86fb65afe2d61c65ec69b952404ad6950
NaCa1814 <lhn18q14fj@gmail.com>
2023-04-11 18:03:30 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install wget -y
-wget $DL_URL
-unzip $Pack_Name.zip
-cd $Open_Pack_Name
+sudo wget $DL_URL
+sudo unzip $Pack_Name.zip
+sudo cd $Open_Pack_Name
 export JAVA11_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/11.0.15-10/x64
 export JAVA18_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/18.0.1-10/x64
 export JAVA8_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/8.0.332-9/x64
